165de26cee62c60584c27bbeded150f08b1ccd86
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-02-12 14:05:48 +0530

    Update addvalues.cpp

--- addvalues.cpp
+++ addvalues.cpp
@@ -1,50 +1,27 @@
 #include<iostream.h>
 #include<conio.h>
-#include<string.h>
-#include<fstream.h>
-#include<stdio.h>
-
-void wordcount()
-{
-  fstream file;
-  file.open("diary1.txt",ios::in|ios::out);
-
-  char ans='y';
-  char word[500];
-
-  while (ans=='y')
-  {
-    cout<<"\n\nEnter the string word by word ";
-    gets(word);
-    file<<word;
-    cout<<"\nDo you want to continue ";
-    cin>>ans;
-  }
-
-    int count=0;
-    file.close();
-    file.open("diary1.txt",ios::in);
-    while(!file.eof())
-    {
-      file.getline(word,3);
-      if (strcmp(word,"my")==0|| strcmp(word,"me")==0)
-      {
-	count++;
-      }
-    }
-
-    cout<<"\n\nThe count is "<<count;
-    file.close();
-
-
-
+void EvenOdd(int T[],int C)
+{	
+for(int i=0;i<C;i++)
+	{
+		if(T[i]%2==0)
+		T[i]+=2;
+		else if(T[i]%2==1)
+		T[i]+=1;
+	}
+	cout<<"New array: ";
+	for(int j=0;j<C;j++)
+	cout<<T[j]<<'\t';
 }
-
-
 void main()
-{
-  clrscr();
-  wordcount();
-  getch();
-
+{	
+clrscr();
+	int T[10],C;
+	cout<<"Enter the size of the array: ";
+	cin>>C;
+	cout<<"Enter the elements of the array:\n";
+	for(int i=0;i<C;i++)
+	cin>>T[i];
+	EvenOdd(T,C);
+	getch();
 }
