7f662efd6d3810164c6cc6f1ac0b2b4a83cf4846
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-02-12 14:06:52 +0530

    Update sumarrays.cpp

--- sumarrays.cpp
+++ sumarrays.cpp
@@ -1,50 +1,30 @@
 #include<iostream.h>
 #include<conio.h>
-#include<string.h>
-#include<fstream.h>
-#include<stdio.h>
-
-void wordcount()
+void SumLast3(int A[2][4],int N,int M)
 {
-  fstream file;
-  file.open("diary1.txt",ios::in|ios::out);
-
-  char ans='y';
-  char word[500];
-
-  while (ans=='y')
-  {
-    cout<<"\n\nEnter the string word by word ";
-    gets(word);
-    file<<word;
-    cout<<"\nDo you want to continue ";
-    cin>>ans;
-  }
-
-    int count=0;
-    file.close();
-    file.open("diary1.txt",ios::in);
-    while(!file.eof())
-    {
-      file.getline(word,3);
-      if (strcmp(word,"my")==0|| strcmp(word,"me")==0)
-      {
-	count++;
-      }
-    }
-
-    cout<<"\n\nThe count is "<<count;
-    file.close();
-
-
-
+	int sum=0;
+	for(int i=0;i<M;i++)
+	{	
+for(int j=0;j<N;j++)
+		{	
+if(A[i][j]%10==3)
+			sum+=A[i][j];
+		}
+	}
+	cout<<"The sum of values ending with 3 is "<<sum;
 }
-
-
 void main()
-{
-  clrscr();
-  wordcount();
-  getch();
-
+{	clrscr();
+	int A[2][4],M=2,N=4;
+	cout<<"Enter the elemens of the array: ";
+	for(int i=0;i<2;i++)
+	{	
+for(int j=0;j<4;j++)
+		{
+			cin>>A[i][j];
+		}
+	}
+	SumLast3(A,N,M);
+	getch();
 }
+
